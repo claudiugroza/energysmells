5fbe25e4212cd4775bb526758a0d421ea75f80d1
Claudiu Groza <groza.claudiu9217@gmail.com>
2017-05-27 19:17:41 +0300

    Change experiment constants

--- app/src/main/java/com/claug/energycodesmells/ExperimentConstants.java
+++ app/src/main/java/com/claug/energycodesmells/ExperimentConstants.java
@@ -2,12 +2,12 @@ package com.claug.energycodesmells;
 
 class ExperimentConstants {
 
-  static final int HMU_TEST_REPETITIONS = 1000000;
-  static final int IGS_TEST_REPETITIONS = 100000000;
+  static final int HMU_TEST_REPETITIONS = 100000;
+  static final int IGS_TEST_REPETITIONS = 10000000;
   static final int MIM_TEST_REPETITIONS = 100000;
 
   // single scenario = 15s
   // 8 min scenario = 480s
-  public static final int SCENARIO_REPETITIONS = 40;
+  public static final int SCENARIO_REPETITIONS = 50;
 
 }
