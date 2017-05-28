c51ec9dfc58ea0b142c016528d3d05c4891dfc99
Claudiu Groza <groza.claudiu9217@gmail.com>
2017-05-28 13:46:34 +0300

    Fix HMU smell

--- app/src/main/java/com/claug/energycodesmells/HMUFragment.java
+++ app/src/main/java/com/claug/energycodesmells/HMUFragment.java
@@ -5,18 +5,17 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import java.util.HashMap;
 
 
 public class HMUFragment extends Fragment {
 
-  private HashMap<Integer, String> map = new HashMap<>();
+  private SparseArray<String> map = new SparseArray<>();
 
   public HMUFragment() {
   }
