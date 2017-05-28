bb195dd817619284ccc3ce324cf74c3534bdbec3
Claudiu Groza <groza.claudiu9217@gmail.com>
2017-05-28 13:38:40 +0300

    Remove public identifier

--- app/src/main/java/com/claug/energycodesmells/ExperimentConstants.java
+++ app/src/main/java/com/claug/energycodesmells/ExperimentConstants.java
@@ -6,6 +6,6 @@ class ExperimentConstants {
   static final int IGS_TEST_REPETITIONS = 100000000;
   static final int MIM_TEST_REPETITIONS = 100000;
 
-  public static final int SCENARIO_REPETITIONS = 120;
+  static final int SCENARIO_REPETITIONS = 120;
 
 }
